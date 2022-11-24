67a0d41ab8730fc993fe2e5019d49e4505a53e47
jmaranguren89@gmail.com <jmaranguren89@gmail.com>
2022-11-24 22:19:03 +0100

    add bibles

--- CalculadoraIMC/app/src/main/java/com/example/calculadoraimcdejose/MainActivity.kt
+++ CalculadoraIMC/app/src/main/java/com/example/calculadoraimcdejose/MainActivity.kt
@@ -2,16 +2,17 @@ package com.example.calculadoraimcdejose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.TextView
-
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
 
     //Inicio variables que usaremos en la app
-    var gender: Boolean? = null
+    var gendermale: Boolean? = null
     var weight: Int? = null
     var height: Int? = null
     lateinit var textCM :TextView
@@ -34,7 +35,20 @@ class MainActivity : AppCompatActivity() {
         rbutonF  = findViewById(R.id.radioButtonFemale)
         submit = findViewById(R.id.btnSubmit)
 
+        submit.setOnClickListener {
 
+            if (!allData()) {
+
+                //llamamos a calculo imc
+                imc()
+            }
+
+            else {
+                //llamamos a snackbar
+                snack()
+            }
+
+        }
 
 
     }
@@ -56,6 +70,41 @@ class MainActivity : AppCompatActivity() {
     }
     //Creamos una funcion que mostrara un snackbar con notificacion.
     fun snack() {
+
+        val mySnackbar = Snackbar.make(findViewById(R.id.rootlayout), R.string.snack,1000 )
+        mySnackbar.show()
+
+    }
+
+    //funcion calculo del imc devolvera un entero que representa los posibles resultados
+    fun imc(): Int {
+
+        var imc: Double = (weight!!/ height!!).toDouble()
+        var imcResultado = null
+
+        if (gendermale!!) {
+
+            if (imc< 18.5) {imcResultado=0}
+            if (18.5<=imc && imc <24.9 ) {imcResultado=1}
+            if (24.9<=imc && imc <30.9) {imcResultado=2}
+            if (30<=imc && imc <35.9) {imcResultado=3}
+            if (35<=imc && imc <39.9 ) {imcResultado=4}
+            if (imc > 39.9 ) {imcResultado=4}
+        }
+
+        if (!gendermale!!) {
+
+            if (imc< 16.5) {imcResultado=0}
+            if (16.5<=imc && imc <22.9 ) {imcResultado=1}
+            if (23.9<=imc && imc <25.9) {imcResultado=2}
+            if (30<=imc && imc <30.9) {imcResultado=3}
+            if (35<=imc && imc <33.9 ) {imcResultado=4}
+            if (imc > 39.9 ) {imcResultado=4}
+
+        }
+
+        return imcResultado!!
+
 
     }
 
